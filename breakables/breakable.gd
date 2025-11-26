92cd323b628b6d5834a6a4bcb8904d19ec99ce48
Maximus Katsoulis <maximuskatsoulis985@gmail.com>
2025-11-25 22:02:26 -0600

    Breakables now modulate to a darker color when in the radius of a bomb

--- breakables/breakable.gd
+++ breakables/breakable.gd
@@ -2,12 +2,16 @@ class_name Breakable
 extends Node2D
 @onready var breakable_sprite: Sprite2D = $BreakableSprite
 @onready var hurtbox_collider: CollisionShape2D = $Hurtbox/HurtboxCollider
+@onready var detector_collider: CollisionShape2D = $ExplosionDetector/DetectorCollider
 @onready var health: Health = $Health
 var shape_component: ShapeComponent = null
+var base_modulate: Color = modulate
+var explosion_detected_modulate: Color = Color(0.5, 0.5, 0.5)
 
 func _ready() -> void:
 	var collision_shape: Shape2D = shape_component.get_shape_collider()
 	hurtbox_collider.shape = collision_shape
+	detector_collider.shape = collision_shape
 	breakable_sprite.texture = shape_component.get_shape_texture()
 	var health_amount = shape_component.get_shape_health()
 	health.set_health(health_amount)
@@ -16,6 +20,15 @@ func _ready() -> void:
 
 
 func _on_health_depleted(health_node: Health) -> void:
-	print("Depleted")
 	if health_node in get_children():
 		queue_free()
+
+
+func _on_explosion_detector_area_entered(area: Area2D) -> void:
+	if area.name == "ExplosionDetectionArea":
+		modulate = explosion_detected_modulate
+
+
+func _on_explosion_detector_area_exited(area: Area2D) -> void:
+	if area.name == "ExplosionDetectionArea":
+		modulate = base_modulate
--- explosives/explosive.gd
+++ explosives/explosive.gd
@@ -1,10 +1,14 @@
 class_name Explosive
 extends Node2D
+@export var keep_detection_active: bool = false
+var detonation_time: float = 1.15
 @onready var explosive_area_shader_box: Sprite2D = $ExplosiveAreaShaderBox
 @onready var explosive_sprite: Sprite2D = $ExplosiveSprite
 @onready var detonation_timer: Timer = $DetonationTimer
 @onready var hitbox_collider: CollisionShape2D = $ExplosionArea/HitboxCollider
-var detonation_time: float = 1.15
+@onready var detection_area_collider: CollisionShape2D = $ExplosionDetectionArea/DetectionAreaCollider
+@onready var explosion_detection_area: Area2D = $ExplosionDetectionArea
+
 
 
 func handle_placed() -> void:
@@ -12,12 +16,16 @@ func handle_placed() -> void:
 	explosive_area_shader_box.visible = false
 	explosive_sprite.z_index = 1
 	detonation_timer.start(detonation_time)
+	if not keep_detection_active:
+		explosion_detection_area.monitorable = false
+		explosion_detection_area.monitoring = false
 
 
 func _on_detonation_timer_timeout() -> void:
 	# TO DO: Handle all explosion effects, particles, sounds, etc...
 	hitbox_collider.disabled = false
 	# A short delay to allow for collision detection with the other areas
-	await get_tree().process_frame
+	for i in range(3):
+		await get_tree().process_frame
 	
 	queue_free()
